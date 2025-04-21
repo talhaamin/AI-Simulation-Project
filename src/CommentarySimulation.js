27f8a2c13d302f77031fcd00e2365417ef277859
Talha Amin Shaikh <talha149@gmail.com>
2025-04-21 20:26:40 +0500

    package JSON commited

--- src/CommentarySimulation.js
+++ src/CommentarySimulation.js
@@ -1,7 +1,3 @@
-
-// Placeholder: Extended functionality such as Timeline, Voice Playback,
-// Feedback UI, Custom Commentary Input, Story Mode will be implemented here.
-
 export default function CommentarySimulation() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
